9dfccdc3c955bb158735ac87f630622229c83367
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 19:15:05 +0200

    improved new feature on LukasPC

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("I wrote this on 'Desktop JPPG135'");
 		System.out.println("And I wrote this back again on LukasPC");
 		System.out.println("This is a new feature, so I need a new branch");
+		System.out.println("improvement of new feature");
 	}
 
 	public static void main(String[] args) {
