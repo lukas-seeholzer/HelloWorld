c5c5c516b2e68d627b5392571dff5c88b5554311
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 19:07:00 +0200

    new feature on new branch

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("hello world");
 		System.out.println("I wrote this on 'Desktop JPPG135'");
 		System.out.println("And I wrote this back again on LukasPC");
+		System.out.println("This is a new feature, so I need a new branch");
 	}
 
 	public static void main(String[] args) {
