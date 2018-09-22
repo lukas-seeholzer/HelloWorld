3fe15329cf8f22924656b2d7b59c95bc5eafa15a
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 20:23:22 +0200

    addded a feature from desktopJPPG and deleted second syso

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -4,11 +4,12 @@ public class HelloWorld {
 	
 	public HelloWorld() {
 		System.out.println("hello world");
-		System.out.println("I wrote this on 'Desktop JPPG135'");
+		System.out.println("delete second feature");
 		System.out.println("And I wrote this back again on LukasPC");
 		System.out.println("This is a new feature, so I need a new branch");
 		System.out.println("improvement of new feature");
 		System.out.println("improving it again on desktop jppg135");
+		System.out.println("adding feature from desktop");
 	}
 
 	public static void main(String[] args) {
