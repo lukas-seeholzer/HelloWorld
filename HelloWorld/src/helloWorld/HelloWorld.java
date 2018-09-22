f9d8034f54854defd0f9969d5ca3cbe3159fffdc
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 18:59:27 +0200

    wrote a syso on LukasPC

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public HelloWorld() {
 		System.out.println("hello world");
 		System.out.println("I wrote this on 'Desktop JPPG135'");
+		System.out.println("And I wrote this back again on LukasPC");
 	}
 
 	public static void main(String[] args) {
