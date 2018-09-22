e63060be3914a8d9b3a2a2b9ad9ed8364bf3003c
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 18:56:20 +0200

    added syso on tab

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	
 	public HelloWorld() {
 		System.out.println("hello world");
+		System.out.println("I wrote this on 'Desktop JPPG135'");
 	}
 
 	public static void main(String[] args) {
