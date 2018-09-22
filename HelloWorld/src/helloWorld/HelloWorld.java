eb7116dd1a96aaee961446258704b98bc67449d0
Lukas Benjamin Seeholzer <lukas.s@bluemail.ch>
2018-09-22 20:20:29 +0200

    added a new feature from pc and deleted the very first feature

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -3,12 +3,13 @@ package helloWorld;
 public class HelloWorld {
 	
 	public HelloWorld() {
-		System.out.println("hello world");
+		System.out.println("deleting first syso");
 		System.out.println("I wrote this on 'Desktop JPPG135'");
 		System.out.println("And I wrote this back again on LukasPC");
 		System.out.println("This is a new feature, so I need a new branch");
 		System.out.println("improvement of new feature");
 		System.out.println("improving it again on desktop jppg135");
+		System.out.println("adding feature from pc");
 	}
 
 	public static void main(String[] args) {
